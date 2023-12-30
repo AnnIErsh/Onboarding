a1c2c1a5d28c14bed4d378867bf3872732b03248
Anna <annaersh1994@gmail.com>
2023-12-30 23:01:09 +0100

    Initial Commit

---
+++ SuperOnboarding/SuperOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperOnboardingApp.swift
+//  SuperOnboarding
+//
+//  Created by Anna Ershova on 30.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
